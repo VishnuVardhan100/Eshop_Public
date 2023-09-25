cc88423e864ae3da285f1d6bbeb757d5bcf7c741
Vishnu Vardhan DV <jakedragonaire1@gmail.com>
2023-09-25 16:57:58 +0530

    Added Entity,Component and EnableJPARepos in main Added above as project is placed in different pkgs Also removed spring security dependency temporarily

--- eshop-application/src/main/java/com/eshop/eshopapplication/EshopApplication.java
+++ eshop-application/src/main/java/com/eshop/eshopapplication/EshopApplication.java
@@ -5,10 +5,16 @@ import java.util.Locale;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+@EntityScan("com.eshop")
+@ComponentScan(basePackages={"com.eshop"})
+@EnableJpaRepositories("com.eshop.eshoprepository")
 @SpringBootApplication
 public class EshopApplication {
 
