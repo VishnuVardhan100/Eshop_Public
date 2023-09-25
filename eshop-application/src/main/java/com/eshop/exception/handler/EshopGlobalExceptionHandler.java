ca5849eb8e4c98aa9b93c7adcbc53ed6b8ef4f9e
Vishnu Vardhan DV <jakedragonaire1@gmail.com>
2023-09-25 16:59:22 +0530

    Changes to Eshop global exception handler

--- eshop-application/src/main/java/com/eshop/exception/handler/EshopGlobalExceptionHandler.java
+++ eshop-application/src/main/java/com/eshop/exception/handler/EshopGlobalExceptionHandler.java
@@ -1,36 +1,48 @@
-package com.eshop.exception;
+package com.eshop.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.eshop.exception.InvalidInputException;
+import com.eshop.exception.InventoryProductNotFoundException;
+import com.eshop.exception.OrderNotFoundException;
+import com.eshop.exception.UserNotFoundException;
 
 /**
  * Base handler for all exceptions in Eshop Application.
  */
 
-@ControllerAdvice
-public class EshopGlobalExceptionHandler {
+@RestControllerAdvice
+public class EshopGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(value = InvalidInputException.class)
 	public ResponseEntity<Object> invalidInputException(InvalidInputException invalidInputException, WebRequest webRequest){
-		return new ResponseEntity<Object>(invalidInputException, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(invalidInputException.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = InventoryProductNotFoundException.class)
 	public ResponseEntity<Object> inventoryProductNotFoundException(InventoryProductNotFoundException inventoryProductNotFoundException, 
 			WebRequest webRequest){
-		return new ResponseEntity<Object>(inventoryProductNotFoundException, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(inventoryProductNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(value = OrderNotFoundException.class)
 	public ResponseEntity<Object> orderNotFoundException(OrderNotFoundException orderNotFoundException, WebRequest webRequest){
-		return new ResponseEntity<Object>(orderNotFoundException, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(orderNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
 	}
 	@ExceptionHandler(value = UserNotFoundException.class)
 	public ResponseEntity<Object> userNotFoundException(UserNotFoundException userNotFoundException, WebRequest webRequest){
-		return new ResponseEntity<Object>(userNotFoundException, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(userNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
 	}
+
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<Object> eshopGlobalExceptionHandler(Exception exception, WebRequest webRequest){
+		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+	
 	
 }
