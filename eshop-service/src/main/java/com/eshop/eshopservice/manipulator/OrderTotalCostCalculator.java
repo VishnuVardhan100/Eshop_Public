24e99f93715f500ef009ada090c88287b61d3c44
Vishnu Vardhan DV <jakedragonaire1@gmail.com>
2023-09-27 16:49:11 +0530

    Added Inventory,Logistics mapper classes Added order cost calculatorservice class

---
+++ eshop-service/src/main/java/com/eshop/eshopservice/manipulator/OrderTotalCostCalculator.java
@@ -0,0 +1,34 @@
+package com.eshop.eshopservice.manipulator;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.eshop.eshopmodel.logistics.OrderProduct;
+
+/**
+ * Class to calculate cost operations for an order.
+ */
+
+@Service
+public class OrderTotalCostCalculator {
+	
+	
+	/**
+	 * Calculate total cost of a product in order by quantity and base per unit cost
+	 * @param order product quantity
+	 * @param order product unit cost
+	 * @return total cost of an order item in order
+	 */
+	public long getOrderProductTotalCost(long orderProductQuantity, long orderProductUnitCost) {
+		return orderProductQuantity * orderProductUnitCost;
+	}
+	
+	public long getOrderTotalAmount(List<OrderProduct> listOfOrderProducts) {
+		long totalOrderAmount = 0;
+		for(OrderProduct orderProductObject : listOfOrderProducts ) {
+			totalOrderAmount =+ orderProductObject.getOrderProductTotalCost();
+		}
+		return totalOrderAmount;
+	}
+}
--- eshop-service/src/main/java/com/eshop/eshopservice/mapper/LogisticsCustomModelMapper.java
+++ eshop-service/src/main/java/com/eshop/eshopservice/mapper/LogisticsCustomModelMapper.java
@@ -1,9 +1,66 @@
 package com.eshop.eshopservice.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eshop.eshopmodel.logistics.Order;
+import com.eshop.eshopmodel.logistics.OrderDTO;
+import com.eshop.eshopmodel.logistics.OrderProduct;
+import com.eshop.eshopmodel.logistics.OrderProductDTO;
+
+/**
+ *  Custom Model Mapper class to map:
+ * 	Order to OrderDTO and vice versa.
+ * 	OrderProduct to OrderProductDTO and vice versa.
+ */
+
+@Service
 public class LogisticsCustomModelMapper {
 
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	/**
+	 * No arguments constructor
+	 */
 	public LogisticsCustomModelMapper() {
 		
 	}
-
+	
+	/**Map OrderDTO To Order object
+	 * @param OrderDTO object
+	 * @return Order object
+	 */
+	public Order mapOrderDTOToOrder(OrderDTO orderDTOObject) {
+		return modelMapper.map(orderDTOObject, Order.class);
+	}
+	
+	/**
+	 * Map Order To OrderDTO
+	 * @param Order object
+	 * @return OrderDTO object
+	 */
+	public OrderDTO mapOrderToOrderDTO(Order orderObject) {
+		return modelMapper.map(orderObject, OrderDTO.class);
+	}
+	
+	/**
+	 * Map OrderProductDTO To OrderProduct
+	 * @param OrderProductDTO object
+	 * @return OrderProduct object
+	 */
+	public OrderProduct mapOrderProductDTOToOrderProduct(OrderProductDTO orderProductDTOObject) {
+		return modelMapper.map(orderProductDTOObject, OrderProduct.class);
+	}
+	
+	/**
+	 * Map OrderProduct To OrderProductDTO
+	 * @param OrderProduct Object
+	 * @return OrderProductDTO object
+	 */
+	public OrderProductDTO mapOrderProductToOrderProductDTO(OrderProduct orderProductObject) {
+		return modelMapper.map(orderProductObject, OrderProductDTO.class);
+	}
+	
 }
